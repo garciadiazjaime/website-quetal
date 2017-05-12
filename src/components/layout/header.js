04e600ced2b3ebb6f87b1c4d9c3ac17bdd208700
jaime <garciadiazjaime@gmail.com>
2017-05-11 20:59:09 -0500

    fix mobile menu

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -20,16 +20,22 @@ var HeaderSection = React.createClass({
 
     mixins : [ReactRouter.Navigation],
 
+    clickHandler: function() {
+      if(document.getElementsByClassName('navbar-collapse collapse in').length) {
+        document.getElementsByClassName('navbar-toggle')[0].click();
+      }
+    },
+
     render: function() {
         return (
-            <Navbar brand={<Link to="home" className='navbar-brand' title="Quetal | Volver a inicio">Quetal | Volver a inicio</Link>} toggleNavKey={0}  className="navbar-static-top">
+            <Navbar brand={<Link to="home" className='navbar-brand' title="Quetal | Volver a inicio" onClick={this.clickHandler}>Quetal | Volver a inicio</Link>} toggleNavKey={0}  className="navbar-static-top">
                 <CollapsibleNav eventKey={0}> {/* This is the eventKey referenced */}
                   <Nav navbar>
-                    <li><Link to="homepage">Inicio</Link></li>
-                    <li><Link to="coverage">Coberturas</Link></li>
-                    <li><Link to="aboutus">Nosotros</Link></li>
-                    <li><Link to="contact">Contacto</Link></li>
-                    <li><a href={this.props.data.cotizaLink} target="_blank" className="external">Cotiza</a></li>
+                    <li><Link to="homepage" onClick={this.clickHandler}>Inicio</Link></li>
+                    <li><Link to="coverage" onClick={this.clickHandler}>Coberturas</Link></li>
+                    <li><Link to="aboutus" onClick={this.clickHandler}>Nosotros</Link></li>
+                    <li><Link to="contact" onClick={this.clickHandler}>Contacto</Link></li>
+                    <li><a href={this.props.data.cotizaLink} target="_blank" className="external" onClick={this.clickHandler}>Cotiza</a></li>
                   </Nav>
                 </CollapsibleNav>
                 <SocialWidget />
